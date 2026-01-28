f4bbd6fca7fc15dc4b185660087e656d12188de4
RiyazShaik-158 <riyaz.shaik.1481@gmail.com>
2026-01-28 08:25:38 +0530

    feat(customer controller): added customer controller

---
+++ src/main/java/com/learning/sales/controller/CustomerController.java
@@ -0,0 +1,29 @@
+package com.learning.sales.controller;
+
+import com.learning.sales.dto.Customer.CustomerResponseDto;
+import com.learning.sales.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customer")
+@Tag(name = "Customer Controller", description = "Customer API")
+public class CustomerController {
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    private final CustomerService customerService;
+
+    @GetMapping
+    @Operation(summary = "Get All Customers")
+    public List<CustomerResponseDto> getCustomers() {
+        return customerService.getAllCustomers();
+    }
+}
