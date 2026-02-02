fa461d7f364f8a72efa7d4453299099d7f475fbd
RiyazShaik-158 <riyaz.shaik.1481@gmail.com>
2026-02-02 06:38:27 +0530

    feat(customer repo): added missing Repository annotation

--- src/main/java/com/learning/sales/repo/CustomerRepo.java
+++ src/main/java/com/learning/sales/repo/CustomerRepo.java
@@ -2,7 +2,8 @@ package com.learning.sales.repo;
 
 import com.learning.sales.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
-
 }
