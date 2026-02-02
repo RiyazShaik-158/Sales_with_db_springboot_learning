c68c396a82060bb06a68c5332dd31a00f03f2127
RiyazShaik-158 <riyaz.shaik.1481@gmail.com>
2026-02-02 06:41:04 +0530

    feat(sales service): implemented add, edit, and delete sales operations

--- src/main/java/com/learning/sales/service/Impl/SaleServiceImpl.java
+++ src/main/java/com/learning/sales/service/Impl/SaleServiceImpl.java
@@ -1,21 +1,29 @@
 package com.learning.sales.service.Impl;
 
+import com.learning.sales.dto.Sales.SalesRequestDto;
 import com.learning.sales.dto.Sales.SalesResponseDto;
 import com.learning.sales.entity.Sale;
 import com.learning.sales.exception.CustomerNotFoundError;
+import com.learning.sales.repo.CustomerRepo;
+import com.learning.sales.repo.ItemRepo;
 import com.learning.sales.repo.SalesRepo;
 import com.learning.sales.service.SaleService;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class SaleServiceImpl implements SaleService {
 
     private final SalesRepo salesRepo;
+    private final ItemRepo itemRepo;
+    private final CustomerRepo customerRepo;
 
-    public SaleServiceImpl(SalesRepo salesRepo) {
+    public SaleServiceImpl(SalesRepo salesRepo, ItemRepo itemRepo, CustomerRepo customerRepo) {
         this.salesRepo = salesRepo;
+        this.itemRepo = itemRepo;
+        this.customerRepo = customerRepo;
     }
 
     @Override
@@ -43,6 +51,58 @@ public class SaleServiceImpl implements SaleService {
             return salesRepo.findByItemId(itemId).stream().map((SaleServiceImpl::mapEntityToResponse)).toList();
         } else {
             throw new CustomerNotFoundError("Sales not found with item id: " + itemId);
+        }
+    }
+
+    @Override
+    public void addSales(SalesRequestDto salesRequestDto) {
+
+        checkItemAndCustomerExistence(salesRequestDto);
+
+        Sale newSale = new Sale();
+        newSale.setCustomer_id(salesRequestDto.getCustomerId());
+        newSale.setItem_id(salesRequestDto.getItemId());
+        newSale.setDate(new Date());
+        newSale.setQuantity(salesRequestDto.getQuantity());
+        newSale.setPrice(salesRequestDto.getPrice());
+        salesRepo.save(newSale);
+    }
+
+    @Override
+    public void editSales(long id,SalesRequestDto salesRequestDto) {
+        Sale requestedSale = salesRepo.findById(id).orElseThrow(() -> new CustomerNotFoundError("Sales not found with id: " + id));
+
+        checkItemAndCustomerExistence(salesRequestDto);
+
+        requestedSale.setCustomer_id(salesRequestDto.getCustomerId());
+        requestedSale.setItem_id(salesRequestDto.getItemId());
+        requestedSale.setDate(new Date());
+        requestedSale.setQuantity(salesRequestDto.getQuantity());
+        requestedSale.setPrice(salesRequestDto.getPrice());
+        salesRepo.save(requestedSale);
+    }
+
+    @Override
+    public void deleteSales(long id) {
+        if(salesRepo.existsById(id)) {
+            salesRepo.deleteById(id);
+        } else {
+            throw new CustomerNotFoundError("Sales not found with id: " + id);
+        }
+
+    }
+
+    public void checkItemAndCustomerExistence (SalesRequestDto salesRequestDto) {
+        long requestedItemId = salesRequestDto.getItemId();
+        long requestedCustomerId = salesRequestDto.getCustomerId();
+        boolean itemExists = itemRepo.existsById(requestedItemId);
+        boolean customerExists = customerRepo.existsById(requestedCustomerId);
+
+        if(!itemExists) {
+            throw new CustomerNotFoundError("Item not found with id: " + salesRequestDto.getItemId());
+        }
+        if(!customerExists) {
+            throw new CustomerNotFoundError("Customer not found with id: " + salesRequestDto.getCustomerId());
         }
     }
 
