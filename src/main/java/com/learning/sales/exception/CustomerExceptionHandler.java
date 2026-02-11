b3ce5ab858427f59d28f641d28008b43496f9939
RiyazShaik-158 <riyaz.shaik.1481@gmail.com>
2026-02-11 09:17:23 +0530

    feat(exception handler): added handler for MethodArgumentNotValidException

--- src/main/java/com/learning/sales/exception/CustomerExceptionHandler.java
+++ src/main/java/com/learning/sales/exception/CustomerExceptionHandler.java
@@ -2,6 +2,8 @@ package com.learning.sales.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,6 +14,19 @@ public class CustomerExceptionHandler {
     private ResponseEntity<CustomerResponseError> handleError(CustomerBadRequestError customerBadRequestError) {
         CustomerResponseError error = new CustomerResponseError(HttpStatus.BAD_REQUEST.value(), customerBadRequestError.getMessage(), System.currentTimeMillis());
         return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<CustomerResponseError> handleThatError(MethodArgumentNotValidException customerConflictError) {
+        StringBuilder errorMessage = new StringBuilder();
+        customerConflictError.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errorMessage.append(fieldName).append(": ").append(message).append("\n");
+        });
+
+        CustomerResponseError error = new CustomerResponseError(HttpStatus.CONFLICT.value(), errorMessage.toString(), System.currentTimeMillis());
+        return new ResponseEntity<>(error,HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler
