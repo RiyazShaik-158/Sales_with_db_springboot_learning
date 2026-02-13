b313a46cea448a46de847a67bd61550910a1fa35
RiyazShaik-158 <riyaz.shaik.1481@gmail.com>
2026-02-13 10:12:46 +0530

    feat(sales dto): added SalesByCustomerResponseDto

---
+++ src/main/java/com/learning/sales/dto/Sales/SalesByCustomerResponseDto.java
@@ -0,0 +1,40 @@
+package com.learning.sales.dto.Sales;
+
+public class SalesByCustomerResponseDto {
+
+    private String itemName;
+
+    private int quantity;
+
+    private double price;
+
+    public SalesByCustomerResponseDto(String itemName, int quantity, double price) {
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
