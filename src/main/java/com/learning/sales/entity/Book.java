a1a710041ba0f218c6a6973ee3eb04819eae0d38
RiyazShaik-158 <riyaz.shaik.1481@gmail.com>
2026-03-02 23:54:26 +0530

    feat(entity): added Book entity class

---
+++ src/main/java/com/learning/sales/entity/Book.java
@@ -0,0 +1,50 @@
+package com.learning.sales.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "published_year")
+    private Integer publishedYear;
+
+    public Book() {
+    }
+
+    public Book(String title, Integer publishedYear) {
+        this.title = title;
+        this.publishedYear = publishedYear;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getPublishedYear() {
+        return publishedYear;
+    }
+
+    public void setPublishedYear(Integer publishedYear) {
+        this.publishedYear = publishedYear;
+    }
+}
